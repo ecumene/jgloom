a0047cf5788dfd178cdb9eaf4fc9ef43b912cea4
Zach Goethel <zgoethel12@gmail.com>
2016-05-10 19:39:40 -0500

    Format TestGL for beauty :)

--- src/test/java/joogl/TestGL.java
+++ src/test/java/joogl/TestGL.java
@@ -15,28 +15,27 @@ public class TestGL
 	// Framebuffer's render texture
 	public Texture renderTexture;
 
-    @Test
-    public void testGLUtils()
-    {
-    	// Creates a default context manager
-    	contextManager = new ContextManager();
-    	contextManager.createContext();
-    	
-    	// Creates a new framebuffer and gets texture
-    	framebuffer = new Framebuffer(800, 450);
-    	renderTexture = framebuffer.getRenderTexture();
-    	
-    	// Demonstrates maintaining context and
-    	// updating context window data
-    	for (long l = 0; !contextManager.shouldWindowClose(); l ++)
-    	{
-    		contextManager.setTitle("" + l);
-    		contextManager.update();
-    	}
+	@Test
+	public void testGLUtils()
+	{
+		// Creates a default context manager
+		contextManager = new ContextManager();
+		contextManager.createContext();
+		// Creates a new framebuffer and gets texture
+		framebuffer = new Framebuffer(800, 450);
+		renderTexture = framebuffer.getRenderTexture();
 
-    	// Destroys the framebuffer (and texture)
-    	framebuffer.destroy();
-    	// Destroys the context manager
-    	contextManager.destroyContext();
-    }
+		// Demonstrates maintaining context and
+		// updating context window data
+		for (long l = 0; !contextManager.shouldWindowClose(); l++)
+		{
+			contextManager.setTitle("" + l);
+			contextManager.update();
+		}
+
+		// Destroys the framebuffer (and texture)
+		framebuffer.destroy();
+		// Destroys the context manager
+		contextManager.destroyContext();
+	}
 }
