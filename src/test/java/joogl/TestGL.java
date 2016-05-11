08c7c982fec836fe9c66409e7c0efba7ec175276
Zach Goethel <zgoethel12@gmail.com>
2016-05-10 19:27:07 -0500

    Add Framebuffer and simple Texture and update TestGL

--- src/test/java/joogl/TestGL.java
+++ src/test/java/joogl/TestGL.java
@@ -3,22 +3,28 @@ package joogl;
 import org.junit.Test;
 
 import joogl.utils.ContextManager;
+import joogl.utils.Framebuffer;
+import joogl.utils.Texture;
 
 public class TestGL
 {
-    @Test
-    public void testSharedLibraries()
-    {
-    	// Loads the native libraries
-        SharedLibraryLoader.load();
-    }
+	// Context manager for window
+	public ContextManager contextManager;
+	// Framebuffer for rendering
+	public Framebuffer framebuffer;
+	// Framebuffer's render texture
+	public Texture renderTexture;
 
     @Test
-    public void testContextManager()
+    public void testGLUtils()
     {
     	// Creates a default context manager
-    	ContextManager contextManager = new ContextManager();
+    	contextManager = new ContextManager();
     	contextManager.createContext();
+    	
+    	// Creates a new framebuffer and gets texture
+    	framebuffer = new Framebuffer(800, 450);
+    	renderTexture = framebuffer.getRenderTexture();
     	
     	// Demonstrates maintaining context and
     	// updating context window data
@@ -27,7 +33,9 @@ public class TestGL
     		contextManager.setTitle("" + l);
     		contextManager.update();
     	}
-    	
+
+    	// Destroys the framebuffer (and texture)
+    	framebuffer.destroy();
     	// Destroys the context manager
     	contextManager.destroyContext();
     }
