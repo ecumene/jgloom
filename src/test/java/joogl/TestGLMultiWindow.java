c94a6911aa84ce1703e01b2e3226623d718ec359
Zach Goethel <zgoethel12@gmail.com>
2016-05-12 22:16:35 -0500

    Remove flop from TestGLMultiWindow

--- src/test/java/joogl/TestGLMultiWindow.java
+++ src/test/java/joogl/TestGLMultiWindow.java
@@ -6,7 +6,6 @@ import joogl.glfw.GLFWWindow;
 import joogl.glfw.GLFWWindowContainer;
 import joogl.glfw.GLFWWindows;
 import org.junit.Test;
-import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLContext;
 
 public class TestGLMultiWindow
@@ -27,27 +26,19 @@ public class TestGLMultiWindow
 		window2 = new GLFWWindowContainer(GLFWWindows.createWindow(800, 450, "Window 2", 0L, 0L));
 		GLFWWindows.makeContextCurrent(window2);
 		GLContext.createFromCurrent();
-		// Track switched colors
-		boolean flop = false;
 
 		// Running while loops in JUnit tests are... Not fun. (it will spam you with GLFW windows)
 		// Rendering to the screen a single frame and closing is good enough!
 
-		// Draw red or green to window 1 depending on flop
+		// Draw red to window 1 depending on flop
 		GLFWWindows.makeContextCurrent(window1);
-		if (flop)
 			glClearColor(1, 0, 0, 1);
-		else
-			glClearColor(0, 1, 0, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 		((GLFWWindowContainer) window1).swapBuffers();
 
-		// Draw green or red to window 2 depending on flop
+		// Draw green to window 2 depending on flop
 		GLFWWindows.makeContextCurrent(window2);
-		if (flop)
 			glClearColor(0, 1, 0, 1);
-		else
-			glClearColor(1, 0, 0, 1);
 		glClear(GL_COLOR_BUFFER_BIT);
 		((GLFWWindowContainer) window2).swapBuffers();
 
