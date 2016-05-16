b592070df3e842ab5802aa1c66edab2312d65c82
Zach Goethel <zgoethel12@gmail.com>
2016-05-16 16:20:52 -0500

    Wrote GLSLShader wrapping and utilities

---
+++ src/main/java/jgloom/GLSLCompileException.java
@@ -0,0 +1,17 @@
+package jgloom;
+
+import jgloom.gl.glsl.GLSLShader;
+
+/**
+ * Indicates that an error occurred while compiling a {@link GLSLShader},
+ * usually an error in the GLSL syntax
+ */
+public class GLSLCompileException extends GLNativeException
+{
+	private static final long serialVersionUID = 1L;
+
+	public GLSLCompileException(String desc)
+	{
+		super(desc);
+	}
+}
---
+++ src/main/java/jgloom/common/gl/glsl/GLSLShaderContainer.java
@@ -0,0 +1,64 @@
+package jgloom.common.gl.glsl;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL20;
+
+import jgloom.GLSLCompileException;
+import jgloom.gl.glsl.GLSLShader;
+
+/**
+ * A shell class containing functions for manipulating a given
+ * {@link GLSLShader}
+ */
+public class GLSLShaderContainer implements GLSLShader
+{
+	private GLSLShader shaderInstance;
+	
+	/**
+	 * Initializes the GLSL shader container
+	 * 
+	 * @param windowInstance The GLFW Window to track and contain
+	 */
+	public GLSLShaderContainer(GLSLShader shaderInstance)
+	{
+		this.shaderInstance = shaderInstance;
+	}
+	
+	/**
+	 * Sets the source of the shader to the given GLSL source code
+	 * 
+	 * @param source
+	 */
+	public void uploadSource(String source)
+	{
+		GL20.glShaderSource(shaderInstance.getGLSLShader(), source);
+	}
+	
+	/**
+	 * Compiles the shader's GLSL source code and checks for errors
+	 * 
+	 * @throws GLSLCompileException When the shader can not successfully compile
+	 */
+	public void compileShader()
+	{
+		GL20.glCompileShader(shaderInstance.getGLSLShader());
+		int compileStatus = GL20.glGetShaderi(shaderInstance.getGLSLShader(), GL20.GL_COMPILE_STATUS);
+		
+		if (compileStatus == GL11.GL_FALSE)
+		{
+			String errorLog = GL20.glGetShaderInfoLog(shaderInstance.getGLSLShader());
+			throw new GLSLCompileException(errorLog);
+		}
+	}
+	
+	@Override
+	public int getGLSLShader() 
+	{
+		return shaderInstance.getGLSLShader();
+	}
+	
+	public GLSLShader getShaderInstance()
+	{
+		return shaderInstance;
+	}
+}
