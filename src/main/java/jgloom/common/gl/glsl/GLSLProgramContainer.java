7982822deb8e2d0b79140333b9846ade8c588e19
ecumene-software <mitchell.hynes@ecumene.xyz>
2016-05-16 22:04:17 -0230

    Documented entire uniform method library
    Hello carpal tunnel my old friend

--- src/main/java/jgloom/common/gl/glsl/GLSLProgramContainer.java
+++ src/main/java/jgloom/common/gl/glsl/GLSLProgramContainer.java
@@ -76,44 +76,25 @@ public class GLSLProgramContainer implements GLSLProgram {
 		return programInstance;
 	}
 
-    // I'm putting these at the bottom for convenience
-
-    // My hands hurt just reading this ...
-    //TODO: document this... whenever
-    //TODO: Add matrixi and matrixd methods and integer uploads + double uploads
-    //TODO: Add string versions of uniforms for them
-
+    /**
+     * Find the given uniform location
+     * @param name The name of the uniform
+     * @return The uniform's location 
+     */
     public int getUniformLocation(String name){
         return GL20.glGetUniformLocation(getGLSLProgram(), name);
     }
 
-    public void setUniform(int location, int ... values){
+    /**
+     * Uploads the array of integers to the given uniform location (4 values maximum, 1 minimum)
+     * @param location  The uniform location of the matrix
+     * @param values    The array value to set
+     */
+    public void setUniformf(int location, float... values) {
         if (values.length > 4)
             throw new GLNativeException("Uniform component cannot have more than 4 components");
-
-        switch(values.length)
-        {
-            case 1:
-                GL20.glUniform1i(location, values[0]);
-                break;
-
-            case 2:
-                GL20.glUniform2i(location, values[0], values[1]);
-                break;
-
-            case 3:
-                GL20.glUniform3i(location, values[0], values[1], values[2]);
-                break;
-
-            case 4:
-                GL20.glUniform4i(location, values[0], values[1], values[2], values[3]);
-                break;
-        }
-    }
-
-    public void setUniform(int location, float... values) {
-        if (values.length > 4)
-            throw new GLNativeException("Uniform component cannot have more than 4 components");
+        if (values.length == 0 )
+            throw new GLNativeException("Uniform component must have at least 1 component");
 
         switch (values.length)
         {
@@ -135,70 +116,239 @@ public class GLSLProgramContainer implements GLSLProgram {
         }
     }
 
-    public void setUniform(int location, boolean transpose, Matrix4f value) {
+    /**
+     * Uploads the array of integers to the given uniform location (4 values maximum, 1 minimum)
+     * @param location  The uniform location of the matrix
+     * @param values    The array value to set
+     */
+    public void setUniformi(int location, int ... values) {
+        if (values.length > 4 )
+            throw new GLNativeException("Uniform component cannot have more than 4 components");
+        if (values.length == 0 )
+            throw new GLNativeException("Uniform component must have at least 1 component");
+
+        switch (values.length)
+        {
+            case 1:
+                GL20.glUniform1i(location, values[0]);
+                break;
+
+            case 2:
+                GL20.glUniform2i(location, values[0], values[1]);
+                break;
+
+            case 3:
+                GL20.glUniform3i(location, values[0], values[1], values[2]);
+                break;
+
+            case 4:
+                GL20.glUniform4i(location, values[0], values[1], values[2], values[3]);
+                break;
+        }
+    }
+
+    /**
+     * Converts the given matrix to a float buffer and uploads it to GL20
+     * @param location  The uniform location of the matrix
+     * @param transpose Transpose before uploading (does nothing to original matrix)
+     * @param value     The value to upload
+     */
+    public void setUniformf(int location, boolean transpose, Matrix3f value) {
+        GL20.glUniformMatrix3fv(location, transpose, value.get(BufferUtils.createFloatBuffer(3*3)));
+    }
+
+    /**
+     * Converts the given matrix to a float buffer and uploads it to GL20
+     * @param location  The uniform location of the matrix
+     * @param transpose Transpose before uploading (does nothing to original matrix)
+     * @param value     The value to upload
+     */
+    public void setUniformf(int location, boolean transpose, Matrix4f value) {
         GL20.glUniformMatrix4fv(location, transpose, value.get(BufferUtils.createFloatBuffer(3*3)));
     }
 
     // Convenience methods
 
-    public void setUniform(int location, boolean transpose, Matrix3f value) {
-        GL20.glUniformMatrix3fv(location, transpose, value.get(BufferUtils.createFloatBuffer(3*3)));
+    /**
+     * Sets a uniform using the given parameters
+     * @param name   The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param values The values to set
+     */
+    public void setUniformi(String name, int... values) {
+        setUniformi(getUniformLocation(name), values);
     }
 
-    public void setUniform(String name, int... values) {
-        setUniform(getUniformLocation(name), values);
+    /**
+     * Sets a uniform using the given parameters
+     * @param location  The vector to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The values to set (x,y)
+     */
+    public void setUniformi(int location, Vector2i value) {
+        setUniformi(location, value.x, value.y);
     }
 
-    public void setUniform(String name, float... values) {
-        setUniform(getUniformLocation(name), values);
+    /**
+     * Sets a uniform using the given parameters
+     * @param location  The vector to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The values to set (x,y,z)
+     */
+    public void setUniformi(int location, Vector3i value) {
+        setUniformi(location, value.x, value.y, value.z);
     }
 
-    public void setUniform(int location, Vector2f value) {
-        setUniform(location, value.x, value.y);
+    /**
+     * Sets a uniform using the given parameters
+     * @param location  The vector to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The values to set (x,y,z,w)
+     */
+    public void setUniformi(int location, Vector4i value) {
+        setUniformi(location, value.x, value.y, value.z, value.w);
     }
 
-    public void setUniform(int location, Vector3f value) {
-        setUniform(location, value.x, value.y, value.z);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name  The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value The vector to set (x,y)
+     */
+    public void setUniformi(String name, Vector2i value) {
+        setUniformi(name, value.x, value.y);
     }
 
-    public void setUniform(int location, Vector4f value) {
-        setUniform(location, value.x, value.y, value.z, value.w);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name  The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value The vector to set (x,y,z)
+     */
+    public void setUniformi(String name, Vector3i value) {
+        setUniformi(name, value.x, value.y, value.z);
     }
 
-    public void setUniform(String name, Vector2f value) {
-        setUniform(name, value.x, value.y);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name  The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value The vector to set (x,y,z,w)
+     */
+    public void setUniformi(String name, Vector4i value) {
+        setUniformi(name, value.x, value.y, value.z, value.w);
     }
 
-    public void setUniform(String name, Vector3f value) {
-        setUniform(name, value.x, value.y, value.z);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name   The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param values The values to set
+     */
+    public void setUniformf(String name, float... values) {
+        setUniformf(getUniformLocation(name), values);
     }
 
-    public void setUniform(String name, Vector4f value) {
-        setUniform(name, value.x, value.y, value.z, value.w);
+    /**
+     * Sets a uniform using the given parameters
+     * @param location  The vector to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The values to set (x,y)
+     */
+    public void setUniformf(int location, Vector2f value) {
+        setUniformf(location, value.x, value.y);
     }
 
-    public void setUniform(int location, Matrix3f value) {
-        setUniform(location, false, value);
+    /**
+     * Sets a uniform using the given parameters
+     * @param location  The vector to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The values to set (x,y,z)
+     */
+    public void setUniformf(int location, Vector3f value) {
+        setUniformf(location, value.x, value.y, value.z);
     }
 
-    public void setUniform(int location, Matrix4f value) {
-        setUniform(location, false, value);
+    /**
+     * Sets a uniform using the given parameters
+     * @param location  The vector to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The values to set (x,y,z,w)
+     */
+    public void setUniformf(int location, Vector4f value) {
+        setUniformf(location, value.x, value.y, value.z, value.w);
     }
 
-    public void setUniform(String name, boolean transpose, Matrix3f value) {
-        setUniform(getUniformLocation(name), transpose, value);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name  The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value The vector to set (x,y)
+     */
+    public void setUniformf(String name, Vector2f value) {
+        setUniformf(name, value.x, value.y);
     }
 
-    public void setUniform(String name, Matrix3f value) {
-        setUniform(name, false, value);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name  The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value The vector to set (x,y,z)
+     */
+    public void setUniformf(String name, Vector3f value) {
+        setUniformf(name, value.x, value.y, value.z);
     }
 
-    public void setUniform(String name, boolean transpose, Matrix4f value) {
-        setUniform(getUniformLocation(name), transpose, value);
+    /**
+     * Sets a uniform using the given parameters
+     * @param name  The name to look up with {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value The vector to set (x,y,z,w)
+     */
+    public void setUniformf(String name, Vector4f value) {
+        setUniformf(name, value.x, value.y, value.z, value.w);
     }
 
-    public void setUniform(String name, Matrix4f value) {
-        setUniform(name, false, value);
+    /**
+     * Sets the given uniform matrix
+     * @param location  The matrix to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The matrix value to upload
+     */
+    public void setUniformf(int location, Matrix3f value) {
+        setUniformf(location, false, value);
+    }
+
+    /**
+     * Sets the given uniform matrix
+     * @param location  The matrix to set (given a uniform location {@link GLSLProgramContainer#getUniformLocation(String)}
+     * @param value     The matrix value to upload
+     */
+    public void setUniformf(int location, Matrix4f value) {
+        setUniformf(location, false, value);
+    }
+
+    /**
+     * Sets the given uniform matrix
+     * @param name      The matrix to set
+     * @param transpose Transpose the matrix before uploading (doesn't change given matrix)
+     * @param value     The matrix value to upload
+     */
+    public void setUniformf(String name, boolean transpose, Matrix3f value) {
+        setUniformf(getUniformLocation(name), transpose, value);
+    }
+
+    /**
+     * Sets the given uniform matrix
+     * @param name      The matrix to set
+     * @param value     The matrix value to upload
+     */
+    public void setUniformf(String name, Matrix3f value) {
+        setUniformf(name, false, value);
+    }
+
+    /**
+     * Sets the given uniform matrix
+     * @param name      The matrix to set
+     * @param transpose Transpose the matrix before uploading (doesn't change given matrix)
+     * @param value     The matrix value to upload
+     */
+    public void setUniformf(String name, boolean transpose, Matrix4f value) {
+        setUniformf(getUniformLocation(name), transpose, value);
+    }
+
+    /**
+     * Sets the given uniform matrix
+     * @param name      The matrix to set
+     * @param value     The matrix value to upload
+     */
+    public void setUniformf(String name, Matrix4f value) {
+        setUniformf(name, false, value);
     }
 
 }
