69de2bb04893bb1f5d745f5cb58adc7420708cf3
ecumene-software <mitchell.hynes@ecumene.xyz>
2016-05-28 23:02:09 -0230

    Testing enhancements

--- src/test/java/jgloom/TestGLMultiWindow.java
+++ src/test/java/jgloom/TestGLMultiWindow.java
@@ -7,6 +7,8 @@ import jgloom.glfw.GLFWWindow;
 import jgloom.common.glfw.GLFWWindowContainer;
 
 import org.junit.Test;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GLContext;
 
 public class TestGLMultiWindow {
@@ -17,6 +19,7 @@ public class TestGLMultiWindow {
     public void testGLMultiWindow() {
         SharedLibraryLoader.load();
         GLFWWindow.init();
+        GLFWWindow.hint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
         window1 = new GLFWWindowContainer(GLFWWindow.createWindow(800, 450, "Window 1", 0L, 0L));
         GLFWWindow.makeContextCurrent(window1);
         GLContext.createFromCurrent();
--- src/test/java/jgloom/TestGLUtil.java
+++ src/test/java/jgloom/TestGLUtil.java
@@ -4,6 +4,7 @@ import jgloom.common.SharedLibraryLoader;
 import jgloom.common.glfw.GLFWWindowContainer;
 import jgloom.glfw.GLFWWindow;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GLContext;
 
@@ -20,9 +21,16 @@ public class TestGLUtil {
         GLFWWindow.hint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
         GLFWWindow.hint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
         GLFWWindow.hint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+        GLFWWindow.hint(GLFW.GLFW_VISIBLE,               GL11.GL_FALSE);
         GLFWWindow.hint(GLFW.GLFW_OPENGL_PROFILE,        GLFW.GLFW_OPENGL_CORE_PROFILE);
         GLFWWindow.hint(GLFW.GLFW_RESIZABLE,             GL11.GL_FALSE);
         GLFWWindow.makeContextCurrent(window);
+        GLFWWindow.setErrorCallback(new GLFWErrorCallback() {
+            @Override
+            public void invoke(int error, long description) {
+                throw new GLNativeException("GLFW Error: " + error + " description: " + description);
+            }
+        });
 
         GLContext.createFromCurrent();
     }
