eaa883e0c13228a1ac15822f7b7aed2f2308728c
Zach Goethel <zgoethel12@gmail.com>
2016-06-20 21:38:31 -0500

    ResourceLoader individual and whole read
    For use with loading shader source among other things

---
+++ src/test/java/jgloom/io/resources/TestResourceReader.java
@@ -0,0 +1,29 @@
+package jgloom.io.resources;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * Class for testing the {@link TestResourceReader} and its ability to load resources
+ */
+public class TestResourceReader {
+    @Test
+    public void testIndividualRead() throws IOException {
+        ResourceLineHandler handler = (String line) -> {
+            System.out.println("Read line: " + line);
+        };
+
+        Resource testText = new ClasspathResource("Text/test_text.txt");
+        ResourceReader.readIndividualLines(testText, handler);
+        System.out.println();
+    }
+    
+    @Test
+    public void testWholeRead() throws IOException {
+        Resource testText = new ClasspathResource("text/test_text.txt");
+        System.out.println(ResourceReader.readWhole(testText));
+        testText.getInputStream().close();
+        System.out.println();
+    }
+}
