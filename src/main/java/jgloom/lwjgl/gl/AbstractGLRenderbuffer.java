f1e4f36a1aab778565a40014205e12523ebe5ca0
ecumene-software <mitchell.hynes@ecumene.xyz>
2016-06-22 01:28:00 -0230

    GL functions for renderbuffer

--- src/main/java/jgloom/lwjgl/gl/AbstractGLRenderbuffer.java
+++ src/main/java/jgloom/lwjgl/gl/AbstractGLRenderbuffer.java
@@ -1,6 +1,9 @@
 package jgloom.lwjgl.gl;
 
 import jgloom.gl.GLRenderbuffer;
+import jgloom.gl.functions.renderbuffer.GLFRenderbufferGetParameter;
+import jgloom.gl.functions.renderbuffer.GLFRenderbufferStorage;
+import jgloom.gl.functions.renderbuffer.GLFRenderbufferStorageMultisample;
 
 /**
  * Renderbuffer Objects are OpenGL Objects that contain images. They are created and used specifically with Framebuffer
@@ -9,9 +12,10 @@ import jgloom.gl.GLRenderbuffer;
  * reading depth back in a second shader pass), enable Textures instead. Renderbuffer objects also natively accommodate
  * Multisampling (MSAA).
  *
- * AbstractRenderBuffer methods do not take targets because their target is always GL_RENDERBUFFER
+ * AbstractRenderbuffer methods do not take targets because their target is always GL_RENDERBUFFER
  */
-public abstract class AbstractGLRenderbuffer implements GLRenderbuffer {
+public abstract class AbstractGLRenderbuffer implements GLFRenderbufferGetParameter, GLFRenderbufferStorage,
+        GLFRenderbufferStorageMultisample {
     private GLRenderbuffer renderbufferInstance;
 
     /**
@@ -21,35 +25,6 @@ public abstract class AbstractGLRenderbuffer implements GLRenderbuffer {
     public AbstractGLRenderbuffer(GLRenderbuffer renderbufferInstance){
         this.renderbufferInstance = renderbufferInstance;
     }
-
-    /**
-     * Bind a renderbuffer to a renderbuffer target
-     */
-    public abstract void bind();
-
-    /**
-     * Establish data storage, format and dimensions of a renderbuffer object's image
-     * @param internalformat Specifies the internal format to enable for the renderbuffer object's image.
-     * @param width          Specifies the width of the renderbuffer, in pixels.
-     * @param height         Specifies the height of the renderbuffer, in pixels.
-     */
-    public abstract void storage(int internalformat, int width, int height);
-
-    /**
-     * Establish data storage, format, dimensions and sample count of a renderbuffer object's image
-     */
-    public abstract void storageMultisample(int samples, int internalformat, int width, int height);
-
-    /**
-     * @param pname The pname
-     * @return the renderbuffer's parameter value
-     */
-    public abstract int getParameter(int pname);
-
-    /**
-     * Deletes the renderbuffer and frees up memory
-     */
-    public abstract void delete();
 
     @Override
     public int getRenderBuffer() {
