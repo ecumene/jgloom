5b2824148dc47df08d3f4f6b14a9eaaff895fe93
ecumene-software <mitchell.hynes@ecumene.xyz>
2016-07-01 11:19:03 -0230

    GL functions for shaders

---
+++ src/main/java/jgloom/gl/functions/glslshader/GLSLFShaderUploadSource.java
@@ -0,0 +1,19 @@
+package jgloom.gl.functions.glslshader;
+
+/**
+ * Functions for GLSL shaders
+ * @see <a href="https://www.opengl.org/wiki/Shader"></a>
+ */
+public interface GLSLFShaderUploadSource extends GLSLFShader {
+    /**
+     * Sets the source code in shader​ to the source code in the array of strings specified by string​. Any source code
+     * previously stored in the shader object is completely replaced. The number of strings in the array is specified
+     * by count​. If length​ is NULL, each string is assumed to be null terminated. If length​ is a value other than NULL,
+     * it points to an array containing a string length for each of the corresponding elements of string​. Each element in
+     * the length​ array may contain the length of the corresponding string (the null character is not counted as part of
+     * the string length) or a value less than 0 to indicate that the string is null terminated. The source code strings
+     * are not scanned or parsed at this time; they are simply copied into the specified shader object.
+     * @param source The source to replace the old shader with
+     */
+    public void uploadSource(String source);
+}
--- src/main/java/jgloom/lwjgl/gl/glsl/GLSLShaderContainer.java
+++ src/main/java/jgloom/lwjgl/gl/glsl/GLSLShaderContainer.java
@@ -1,72 +1,40 @@
 package jgloom.lwjgl.gl.glsl;
 
-import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
 import jgloom.gl.glsl.GLSLShader;
 
 /**
- * A shell class containing functions for manipulating a given
+ * A shell class containing LWJGL functions for manipulating a given
  * {@link GLSLShader}
  */
-public class GLSLShaderContainer implements GLSLShader {
-    private GLSLShader shaderInstance;
-
+public class GLSLShaderContainer extends AbstractGLSLShader {
     /**
-     * Initializes the GLSL shader container
+     * Initializes the LWJGL GLSL shader container
      * @param shaderInstance The GLSL shader to track and contain
      */
     public GLSLShaderContainer(GLSLShader shaderInstance) {
-        this.shaderInstance = shaderInstance;
+        super(shaderInstance);
     }
 
-    /**
-     * Sets the source code in shader​ to the source code in the array of strings specified by string​. Any source code
-     * previously stored in the shader object is completely replaced. The number of strings in the array is specified
-     * by count​. If length​ is NULL, each string is assumed to be null terminated. If length​ is a value other than NULL,
-     * it points to an array containing a string length for each of the corresponding elements of string​. Each element in
-     * the length​ array may contain the length of the corresponding string (the null character is not counted as part of
-     * the string length) or a value less than 0 to indicate that the string is null terminated. The source code strings
-     * are not scanned or parsed at this time; they are simply copied into the specified shader object.
-     * @param source The source to replace the old shader with
-     */
+    @Override
     public void uploadSource(String source) {
         GL20.glShaderSource(getGLSLShader(), source);
     }
 
-    /**
-     * Compiles the source code strings that have been stored in the shader object specified by shader​.
-     * @throws GLSLCompileException When the shader can not successfully compile (usually GLSL errors)
-     */
-    public void compileShader() throws GLSLCompileException {
+    @Override
+    public void compile() throws GLSLCompileException {
         GL20.glCompileShader(getGLSLShader());
-        if(!isCompiled()) throw new GLSLCompileException(getLog());
     }
 
-    /** @return If GL_COMPILE_STATUS is not false */
-    public boolean isCompiled(){
-        return GL20.glGetShaderi(getGLSLShader(), GL20.GL_COMPILE_STATUS) != GL11.GL_FALSE;
-    }
-
-    /** @return The shader's log from glGetShaderInfoLog */
+    @Override
     public String getLog(){
         return GL20.glGetShaderInfoLog(getGLSLShader(), GL20.glGetShaderi(getGLSLShader(), GL20.GL_INFO_LOG_LENGTH));
     }
 
-    /**
-     * Frees the memory and invalidates the name associated with the shader object specified by shader. This command
-     * effectively undoes the effects of a call to {@link GLSLShader#createShader(int)}.
-     */
+    @Override
     public void delete() {
         GL20.glDeleteShader(getGLSLShader());
     }
 
-    @Override
-    public int getGLSLShader() {
-        return shaderInstance.getGLSLShader();
-    }
-
-    public GLSLShader getShaderInstance() {
-        return shaderInstance;
-    }
 }
